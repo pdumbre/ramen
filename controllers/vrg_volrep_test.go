4c03d6afea740e1adc8a63a92ca33155b07effc4
pallavi-joshi <pallavi.joshi@in.ibm.com>
2022-05-23 12:34:49 +0530

    When the user doesn't specifya PVC label selector in VRG spec, Ramen VRG should protect all PVs of the namespace

--- controllers/vrg_volrep_test.go
+++ controllers/vrg_volrep_test.go
@@ -48,6 +48,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 			vrcProvisioner:         "manual.storage.com",
 			scProvisioner:          "manual.storage.com",
 			replicationClassLabels: map[string]string{"protection": "ramen"},
+			emptyPVCLabelSelector:	false,
 		}
 		It("populates the S3 store with PVs and starts vrg as primary to check that the PVs are restored", func() {
 			numPVs := 3
@@ -76,6 +77,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 			vrcProvisioner:         "manual.storage.com",
 			scProvisioner:          "manual.storage.com",
 			replicationClassLabels: map[string]string{"protection": "ramen"},
+			emptyPVCLabelSelector:	false,
 		}
 		It("sets up PVCs, PVs and VRGs", func() {
 			for c := 0; c < 5; c++ {
@@ -125,6 +127,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		vrcProvisioner:         "manual.storage.com",
 		scProvisioner:          "manual.storage.com",
 		replicationClassLabels: map[string]string{"protection": "ramen"},
+		emptyPVCLabelSelector:	false,
 	}
 
 	Context("in primary state", func() {
@@ -217,6 +220,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		vrcProvisioner:         "manual.storage.com",
 		scProvisioner:          "manual.storage.com",
 		replicationClassLabels: map[string]string{"protection": "ramen"},
+		emptyPVCLabelSelector:	false,
 	}
 	var vrgStatus2Tests []*vrgTest
 	Context("in primary state status check bound", func() {
@@ -252,6 +256,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		vrcProvisioner:         "manual.storage.com",
 		scProvisioner:          "manual.storage.com",
 		replicationClassLabels: map[string]string{"protection": "ramen"},
+		emptyPVCLabelSelector:	false,
 	}
 	var vrgStatus3Tests []*vrgTest
 	Context("in primary state status check create VRG first", func() {
@@ -285,6 +290,41 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		})
 	})
 
+	// Empty PVC label selector in VRG should select all PVCs in given namespace. 
+
+	vrgTest4Template := &template{
+		ClaimBindInfo:          corev1.ClaimBound,
+		VolumeBindInfo:         corev1.VolumeBound,
+		schedulingInterval:     "1h",
+		storageClassName:       "manual",
+		replicationClassName:   "test-replicationclass",
+		vrcProvisioner:         "manual.storage.com",
+		scProvisioner:          "manual.storage.com",
+		replicationClassLabels: map[string]string{"protection": "ramen"},
+		emptyPVCLabelSelector:	true,
+	}
+	var vrgStatus4Tests []*vrgTest
+	Context("in check empty label selector", func() {
+		It("sets up PVCs, PVs", func() {
+			v := newVRGTestCaseCreateAndStart(4, vrgTest4Template, true, true)
+			vrgStatus4Tests = append(vrgStatus4Tests, v)
+		})
+		It("waits for VRG to create a VR for each PVC bind and checks status", func() {
+			v := vrgStatus4Tests[0]
+			expectedVRCount := len(v.pvcNames)
+			v.waitForVRCountToMatch(expectedVRCount)
+		})
+		It("waits for VRG to status to match", func() {
+			v := vrgStatus4Tests[0]
+			v.promoteVolReps()
+			v.verifyVRGStatusExpectation(true)
+		})
+		It("cleans up after testing", func() {
+			v := vrgStatus4Tests[0]
+			v.cleanup()
+		})
+	})
+
 	// VolumeReplicationClass provisioner and StorageClass provisioner
 	// does not match. VolumeReplication resources should not be created.
 	var vrgScheduleTests []*vrgTest
@@ -297,6 +337,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		vrcProvisioner:         "manual.storage.com",
 		scProvisioner:          "new.storage.com",
 		replicationClassLabels: map[string]string{"protection": "ramen"},
+		emptyPVCLabelSelector:	false,
 	}
 	Context("schedule test, provisioner does not match", func() {
 		It("sets up non-bound PVCs, PVs and then bind them", func() {
@@ -330,6 +371,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		vrcProvisioner:         "manual.storage.com",
 		scProvisioner:          "manual.storage.com",
 		replicationClassLabels: map[string]string{"protection": "ramen"},
+		emptyPVCLabelSelector:	false,
 	}
 	Context("schedule tests schedue does not match", func() {
 		It("sets up non-bound PVCs, PVs and then bind them", func() {
@@ -363,6 +405,7 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 		vrcProvisioner:         "manual.storage.com",
 		scProvisioner:          "manual.storage.com",
 		replicationClassLabels: map[string]string{},
+		emptyPVCLabelSelector:	false,
 	}
 	Context("schedule tests replicationclass does not have labels", func() {
 		It("sets up non-bound PVCs, PVs and then bind them", func() {
@@ -383,23 +426,26 @@ var _ = Describe("Test VolumeReplicationGroup", func() {
 			v.cleanup()
 		})
 	})
+
+
 	// TODO: Add tests to move VRG to Secondary
 	// TODO: Add tests to ensure delete as Secondary (check if delete as Primary is tested above)
 })
 
 type vrgTest struct {
-	uniqueID         string
-	namespace        string
-	pvNames          []string
-	pvcNames         []string
-	vrgName          string
-	storageClass     string
-	replicationClass string
-	pvcLabels        map[string]string
-	pvcCount         int
-	checkBind        bool
-	vrgFirst         bool
-	template         *template
+	uniqueID         	string
+	namespace        	string
+	pvNames          	[]string
+	pvcNames         	[]string
+	vrgName          	string
+	storageClass     	string
+	replicationClass 	string
+	pvcLabels        	map[string]string
+	pvcCount         	int
+	checkBind        	bool
+	vrgFirst         	bool
+	emptyLabelSelector	bool
+	template         	*template
 }
 
 type template struct {
@@ -411,6 +457,7 @@ type template struct {
 	storageClassName       string
 	replicationClassName   string
 	replicationClassLabels map[string]string
+	emptyPVCLabelSelector  bool
 }
 
 //nolint:gosec
@@ -429,19 +476,19 @@ func newVRGTestCaseCreate(pvcCount int, testTemplate *template, checkBind, vrgFi
 	objectNameSuffix := newRandomNamespaceSuffix()
 
 	v := &vrgTest{
-		uniqueID:         objectNameSuffix,
-		namespace:        fmt.Sprintf("envtest-ns-%v", objectNameSuffix),
-		vrgName:          fmt.Sprintf("vrg-%v", objectNameSuffix),
-		storageClass:     testTemplate.storageClassName,
-		replicationClass: testTemplate.replicationClassName,
-		pvcLabels:        make(map[string]string),
-		pvcCount:         pvcCount,
-		checkBind:        checkBind,
-		vrgFirst:         vrgFirst,
-		template:         testTemplate,
+		uniqueID:         	objectNameSuffix,
+		namespace:        	fmt.Sprintf("envtest-ns-%v", objectNameSuffix),
+		vrgName:          	fmt.Sprintf("vrg-%v", objectNameSuffix),
+		storageClass:     	testTemplate.storageClassName,
+		replicationClass: 	testTemplate.replicationClassName,
+		pvcLabels:        	make(map[string]string),
+		pvcCount:         	pvcCount,
+		checkBind:        	checkBind,
+		vrgFirst:         	vrgFirst,
+		template:         	testTemplate,
 	}
 
-	if pvcCount > 0 {
+	if pvcCount > 0 && !testTemplate.emptyPVCLabelSelector {
 		v.pvcLabels["appclass"] = "platinum"
 		v.pvcLabels["environment"] = fmt.Sprintf("dev.AZ1-%v", objectNameSuffix)
 	}
